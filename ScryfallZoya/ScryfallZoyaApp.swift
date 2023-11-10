0adb03ca62d1be3b149abe350b53cb66e6e4e966
AqiilaSM <ashahbani@student.ciputra.ac.id>
2023-11-10 08:01:29 +0700

    Initial Commit

---
+++ ScryfallZoya/ScryfallZoyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScryfallZoyaApp.swift
+//  ScryfallZoya
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ScryfallZoyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
